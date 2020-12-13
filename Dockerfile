8fc75b25a848f6c9cb17fceab91a11ce3e218b44
Ubuntu <negisanket@gmail.com>
2020-12-13 16:11:27 +0000

    Corrected the files

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
-From java:8
+FROM java:8
 
 COPY . /var/www/java
+
+WORKDIR /var/www/java
 
 RUN javac Hello.java
 
 CMD ["java","Hello"]
+
--- Hello.java
+++ Hello.java
@@ -1,6 +1,9 @@
-class Hello
-{
-public static void main(String[] args) {
-System.out.println("Hello");
-}
+class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!"); 
+
+    }
+
 }
